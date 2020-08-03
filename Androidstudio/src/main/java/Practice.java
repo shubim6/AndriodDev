67c3778d2e3410000e0bc7c0b0684a270bc811b2
Shubiksha <Shubiksha@DESKTOP-H89H6B8.home>
2020-08-03 11:09:29 -0400

    Wrote my first program!

---
+++ Androidstudio/src/main/java/Practice.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Shubiksha
+ */
+public class Practice {
+   public static void main(String []args){
+       
+       String fName = "Shubi";
+       String lName = "Murali";
+       System.out.println("My full name is " + fName + " " + lName);
+   }
+}
