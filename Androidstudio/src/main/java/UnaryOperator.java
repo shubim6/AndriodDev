44eaf1d1c52d5cb44f531617e493587d7e421d35
Shubiksha <Shubiksha@DESKTOP-H89H6B8.home>
2020-08-03 14:34:03 -0400

    More Practice

---
+++ Androidstudio/src/main/java/UnaryOperator.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Shubiksha
+ */
+public class UnaryOperator {
+    public static void main(String []args){
+        double num1 = 5.2, resultNumber;
+        System.out.println("+number = " + +number);
+    }
