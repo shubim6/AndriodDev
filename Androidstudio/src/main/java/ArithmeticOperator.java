3c87cc5e232b113b770dcffebc5f4de366278505
Shubiksha <Shubiksha@DESKTOP-H89H6B8.home>
2020-08-03 16:05:19 -0400

    Learning how to code!

---
+++ Androidstudio/src/main/java/ArithmeticOperator.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Shubiksha
+ */
+public class ArithmeticOperator {
+    public static void main(String []args){
+        double num1 = 15.4;
+        double num2 = 13.6;
+        double quotient = num1/num2;
+        System.out.println(quotient);
+        
+    }
+}
