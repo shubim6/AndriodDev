35e06bce1eb557b8642d5aa30624df4f72d73ff5
Shubiksha <Shubiksha@DESKTOP-H89H6B8.home>
2020-08-04 11:08:13 -0400

    'If, else' coding practice!

---
+++ Androidstudio/src/main/java/RelationOperator.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Shubiksha
+ */
+public class RelationOperator {
+    public static void main(String []args){
+        int num1 = 8;
+        int num2 = 21;
+        if (num2>num1){
+            System.out.println(num2 + " " + "is greater than " + " " + num1);
+        }else{
+            System.out.println(num1 + " " + "is less than " + " " + num2);
+        }
+    }
+}
