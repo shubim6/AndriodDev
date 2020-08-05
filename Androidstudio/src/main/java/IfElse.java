3b8298c195c98e21a960f7a6f47ebcc7a37b549e
Shubiksha <Shubiksha@DESKTOP-H89H6B8.home>
2020-08-05 12:41:30 -0400

    'If, Else with numbers' coding practice

---
+++ Androidstudio/src/main/java/IfElse.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Shubiksha
+ */
+public class IfElse {
+    public static void main(String []args){
+        int number = 10;
+        if (number > 0){
+        System.out.println("The number is positive");
+        }else{
+        System.out.println("The number is negative");
+        }
+        int number2 = -10;
+        if (number2 > 0){
+        System.out.println("The number is positive");
+        }else{
+        System.out.println("The number is negative");
+        }
+    }    
+}
