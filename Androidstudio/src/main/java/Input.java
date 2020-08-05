368cb41aadac889863957e0dfbf7b526fe31c262
Shubiksha <Shubiksha@DESKTOP-H89H6B8.home>
2020-08-05 13:02:56 -0400

    Using Scanner package coding practice!

--- Androidstudio/src/main/java/Input.java
+++ Androidstudio/src/main/java/Input.java
@@ -9,11 +9,16 @@
  * @author Shubiksha
  */
 import java.util.Scanner;
-public class Input {
+public class Input{
     public static void main(String []args){
+
         Scanner input = new Scanner(System.in);
+        System.out.println("What is your age?");
+        //print a statement for the user....ex: "What is your favorite number?"
         int number = input.nextInt();
-        System.out.println(number);
+        //print a statement to confirm what the user entered.....ex:"You entered: " + number
+        System.out.println("You enetered + " + number);
+
         input.close();
     }
 }
