bc1b5ae2cbfa81ec8042f94f5a93c3b760bfd7c3
Shubiksha <Shubiksha@DESKTOP-H89H6B8.home>
2020-08-05 13:29:51 -0400

    Using 'Switch' with days of the week coding practice.

---
+++ Androidstudio/src/main/java/Days.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Shubiksha
+ */
+public class Days {
+    public static void main(String []args){
+        int week = 3;
+        String day;
+        switch (week){
+        case 1:
+        day = "Sunday";
+        case 2:
+        day = "Monday";
+        case 3:
+        day = "Tuesday";
+        System.out.println("The third day of the week is " + day);
+            
+        }
+    }
+}
