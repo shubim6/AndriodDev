2465f399b416b8876b669726950ad959b631232e
Shubiksha <Shubiksha@DESKTOP-H89H6B8.home>
2020-08-05 17:34:49 -0400

    Using loops, coding practice.

---
+++ Androidstudio/src/main/java/Repeat.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Shubiksha
+ */
+public class Repeat {
+    public static void main(String []args){
+        for (int number = 0; number <= 100; ++number){
+            System.out.println("Yes " + number);
+        }
+    }
+}
