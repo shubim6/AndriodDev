9c20028e6ccdf3fa85cd380e92774282a2cba297
Shubiksha <Shubiksha@DESKTOP-H89H6B8.home>
2020-08-05 13:00:39 -0400

    'If, Else-if' coding practice

---
+++ Androidstudio/src/main/java/Ladder.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Shubiksha
+ */
+public class Ladder {
+    public static void main(String []args){
+        int number = 0;
+        if (number > 0){
+        System.out.println("The number is positive");
+        }
+            else if(number < 0){
+            System.out.println("The number is negative");
+            }
+        else{
+        System.out.println("The number is 0");
+        }
+    }
+}    
